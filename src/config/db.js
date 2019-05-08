a43db83b152f1f84dc6d5fc6d746ed2aef583aa3
yangdepp <965093975@qq.com>
2019-05-08 22:54:43 +0800

    数据库配置

---
+++ src/config/db.js
@@ -0,0 +1,27 @@
+const env = process.env.NODE_ENV;
+
+let MYSQL_CONF;
+
+if (env === dev) {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'Mysql_2018',
+    port: '3306',
+    database: 'myblog'
+  };
+}
+
+if (env === 'producton') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'Mysql_2018',
+    port: '3306',
+    database: 'myblog'
+  };
+}
+
+module.exports = {
+  MYSQL_CONF
+};
