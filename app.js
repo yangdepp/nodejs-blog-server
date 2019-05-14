99319795ae8c24b3b41834b30a2466e5445a6f99
yangdepu <yangdepu@zdsoft.cn>
2019-05-14 09:25:29 +0800

    cookie登录

--- app.js
+++ app.js
@@ -37,6 +37,19 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = queryString.parse(url.split('?')[1]);
 
+  // 解析cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || ''; // k1=v1;k2=v2;k3=v3
+  cookieStr.split(';').forEach(item => {
+    if (!item) {
+      return;
+    }
+    const arr = item.split('=');
+    const key = arr[0];
+    const val = arr[1];
+    req.cookie[key] = val;
+  });
+
   // 处理post data
   getPostData(req).then(postData => {
     req.body = postData;
--- src/router/user.js
+++ src/router/user.js
@@ -1,16 +1,28 @@
-const { loginCheck } = require('../controller/user');
+const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 const handleUserRouter = (req, res) => {
   const method = req.method;
 
   // 登录
-  if (method === 'POST' && req.path === '/api/user/login') {
-    const { username, password } = req.body;
-    const result = loginCheck(username, password);
+  if (method === 'GET' && req.path === '/api/user/login') {
+    // const { username, password } = req.body;
+    const { username, password } = req.query;
+    const result = login(username, password);
     return result.then(data => {
-      return data.username ? new SuccessModel() : new ErrorModel('登录失败');
+      if (data.username) {
+        res.setHeader('Set-Cookie', `username=${data.username}; path=/`);
+        return new SuccessModel();
+      }
+      return new ErrorModel('登录失败');
     });
+  }
+
+  // 登录验证的测试
+  if (method === 'GET' && req.path === '/api/user/login-test') {
+    return req.cookie.username
+      ? Promise.resolve(new SuccessModel())
+      : Promise.resolve(new ErrorModel('尚未登录'));
   }
 };
 module.exports = handleUserRouter;
